d58407494fbe099cb5dd4f0fc19d463c4544555a
Imran Sarder <imrancluster@gmail.com>
2019-12-11 09:32:45 +0600

    styling layout

--- people/filters.py
+++ people/filters.py
@@ -9,6 +9,7 @@ class MemberFilter(django_filters.FilterSet):
     class Meta:
         model = Member
         fields = {
+            'member_type': ['exact'],
             'name': ['icontains'],
             'registration_no': ['exact'],
             'phone': ['exact']
