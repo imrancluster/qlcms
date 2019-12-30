327d8406b854f8a71a8c6abd6e883cc53ad482e5
Imran Sarder <imrancluster@gmail.com>
2019-12-30 09:36:09 +0600

    fixed jquery issue for ready bank history

--- qlcms/views.py
+++ qlcms/views.py
@@ -1,19 +1,10 @@
-import datetime
-import math
-import random
-import time
-from itertools import chain
-
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.db.models import Max, Count
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 
 from matirbank.helper import get_ready_banks
-from matirbank.models import MatirBank
-from people.models import UserProfile
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -26,21 +17,20 @@ def ready_banks(request):
 
     if request.is_ajax():
         ready_banks = get_ready_banks(request.user, 10)
-        time.sleep(1)
         return render(request, 'matirbank/partial/ready_banks.html', context={
             'banks': ready_banks,
             'total_banks': len(ready_banks)
         })
+
 
 class Dashboard(UserPassesTestMixin, TemplateView):
     template_name = 'home/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ready_banks = get_ready_banks(self.request.user)
 
-        context['banks'] = ready_banks
-        context['total_banks'] = len(ready_banks)
+        context['banks'] = []
+        context['total_banks'] = 0
 
         return context
 
